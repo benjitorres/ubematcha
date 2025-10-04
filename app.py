38d86a98b26dffd868fc7b73b4a62fdd9b25d299
benji <you@example.com>
2025-10-03 23:10:42 -0700

    mash

--- app.py
+++ app.py
@@ -39,5 +39,7 @@ async def transcribe(file: UploadFile = File(...)):
         ],
     }
 
+"""
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+"""
